15639eb596973ea03d2fddf7464adfe13f31aff5
MoayadMaher <its.moayadj@gmail.com>
2024-08-02 02:12:02 +0300

    Update database

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9d406362480e733288dc524c8278355fc7a1e9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b4cad6dee965d1d7281576e2350ead427551ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
